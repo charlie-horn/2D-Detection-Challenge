4f5c301f588954e43cb343bcd58e94c3163416c5
charlie-horn <charlie.horn@queensu.ca>
2020-05-14 16:20:06 +0000

    Added cuda install to setup

--- setup.sh
+++ setup.sh
@@ -18,9 +18,22 @@ mkdir output
 #gcloud auth login chorn6300@gmail.com
 
 pip3 install --upgrade pip
-pip3 install waymo-open-dataset-tf-2-1-0==1.2.0
+#pip3 install waymo-open-dataset-tf-2-1-0==1.2.0
+pip3 install waymo-open-dataset-tf-2-1-0
 pip3 install gcloud
 pip3 install opencv-python
 pip3 install pandas
 pip3 install keras
 
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-ubuntu1604.pin
+sudo mv cuda-ubuntu1604.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1604-10-2-
+local-10.2.89-440.33.01_1.0-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+mv cuda-repo-ubuntu1604-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb ..
+
+export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64:$LD_LIBRARY_PATH
+export PATH=/usr/local/cuda-10.2/bin:$PATH
